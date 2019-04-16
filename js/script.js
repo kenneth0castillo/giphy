f9ccf0859e4cfcf3ff9c7a13b4496fdedfef5046
Kenneth Castillo <kennethc6970@uamusicandart.org>
2019-04-16 21:38:08 +0000

    update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,30 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+      var choice = $("input").val();
+      var true_url = "https://api.giphy.com/v1/gifs/search?q="+choice+"&rating=pg&api_key=dc6zaTOxFJmzC"
+      var url2 = "https://api.giphy.com/v1/gifs/search?q=popup&rating=pg&api_key=dc6zaTOxFJmzC"
+      console.log(true_url)
+      var random = Math.random();
+      if(random >.11){
+            $.ajax({
+            url: true_url,
+            method: "GET",
+            success: function(response) {
+                console.log(response.data[0].images.original.url);
+                var pic = response.data[0].images.original.url;
+                $('.text-center').html("<img src=" + pic + ">");
+            },
+        }); 
+      }else{
+            $.ajax({
+            url: url2,
+            method: "GET",
+            success: function(response) {
+                console.log(response.data[0].images.original.url);
+                var pic = response.data[0].images.original.url;
+                $('.text-center').html("<img src=" + pic + ">");
+            },
+        }); 
+      }
 });
-
